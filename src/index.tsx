1c6570be8db558903772faeb1fc7fdfa9d14fa13
Syed Sadiq <syed.sadiq@royalcyber.com>
2023-01-28 21:21:19 +0530

    changes

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,7 @@ import 'react-app-polyfill/stable';
 
 import * as React from 'react';
 import ReactDOM from 'react-dom/client';
+import Obfuscate from 'react-obfuscate';
 
 // Use consistent styling
 import 'sanitize.css/sanitize.css';
@@ -31,12 +32,34 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
+// Disable right-click
+document.addEventListener('contextmenu', e => e.preventDefault());
+
+function ctrlShiftKey(e, keyCode) {
+  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
+
+document.onkeydown = e => {
+  // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+  if (
+    // @ts-ignore
+    e.keyCode === 123 ||
+    ctrlShiftKey(e, 'I') ||
+    ctrlShiftKey(e, 'J') ||
+    ctrlShiftKey(e, 'C') ||
+    (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+  )
+    return false;
+};
+
 root.render(
+  // <Obfuscate>
   <HelmetProvider>
     <React.StrictMode>
       <App />
     </React.StrictMode>
   </HelmetProvider>,
+  // </Obfuscate>,
 );
 
 // Hot reloadable translation json files
